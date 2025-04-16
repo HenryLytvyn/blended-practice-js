fed6d287146a686ecb9380af182fd3562b40cb13
HenryLytvyn <hennadii.lytvyn@gmail.com>
2025-04-16 20:25:43 +0000

    Deploying to gh-pages from @ HenryLytvyn/blended-practice-js@83ad84e30d9070a7a51d9e2f681e99f9ecfc01d0 🚀

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-import"./assets/styles-B513HK9d.js";import{a as n}from"./assets/vendor-C19taMLP.js";async function p(t){try{return(await n.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data.products}catch(o){alert(o.message)}}async function f(t){try{return(await n.get(`https://dummyjson.com/products/${t}`)).data}catch(o){alert(o.message)}}async function h(){try{return(await n.get("https://dummyjson.com/products/category-list")).data}catch(t){alert(t.message)}}async function b(t,o){try{return(await n.get(`https://dummyjson.com/products/category/${o}?limit=12&skip=${(t-1)*12}`)).data.products}catch(e){alert(e.message)}}function L(t){return t.map(o=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${o}</button>
+import"./assets/styles-eXUVIhJS.js";import{a}from"./assets/vendor-C19taMLP.js";async function g(t){try{return(await a.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data.products}catch(e){alert(e.message)}}async function $(t){try{return(await a.get(`https://dummyjson.com/products/${t}`)).data}catch(e){alert(e.message)}}async function v(){try{return(await a.get("https://dummyjson.com/products/category-list")).data}catch(t){alert(t.message)}}async function P(t,e){try{return(await a.get(`https://dummyjson.com/products/category/${e}?limit=12&skip=${(t-1)*12}`)).data.products}catch(o){alert(o.message)}}async function _(){try{return(await a.get("https://dummyjson.com/products")).data.total}catch(t){alert(t.message)}}async function f(t){try{return(await a.get(`https://dummyjson.com/products/category/${t}`)).data.products.length}catch(e){alert(e.message)}}function A(t){return t.map(e=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${e}</button>
  </li>
-`).join("")}function d(t){return t.map(({id:o,images:e,description:c,title:a,brand:g,category:_,price:y})=>`
-  <li class="products__item" data-id="${o}">
-    <img class="products__image" src="${e[0]}" alt="${c}"/>
-    <p class="products__title">${a}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand: ${g}</span></p>
-    <p class="products__category">Category: ${_}</p>
-    <p class="products__price">Price: ${y}</p>
+`).join("")}function u(t){return t.map(({id:e,images:o,description:n,title:i,brand:L,category:C,price:M})=>`
+  <li class="products__item" data-id="${e}">
+    <img class="products__image" src="${o[0]}" alt="${n}"/>
+    <p class="products__title">${i}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${L}</span></p>
+    <p class="products__category">Category: ${C}</p>
+    <p class="products__price">Price: ${M}</p>
  </li>
-`).join("")}function C({images:t,description:o,title:e,tags:c,price:a}){return`<img class="modal-product__img" src="${t[0]}" alt="${c}" />
+`).join("")}function j({images:t,description:e,title:o,tags:n,price:i}){return`<img class="modal-product__img" src="${t[0]}" alt="${n}" />
       <div class="modal-product__content">
-        <p class="modal-product__title">${e}</p>
-        <ul class="modal-product__tags">${c}</ul>
-        <p class="modal-product__description">${o}</p>
+        <p class="modal-product__title">${o}</p>
+        <ul class="modal-product__tags">${n}</ul>
+        <p class="modal-product__description">${e}</p>
         <p class="modal-product__shipping-information">Shipping:</p>
         <p class="modal-product__return-policy">Return Policy:</p>
-        <p class="modal-product__price">Price: ${a}</p>
+        <p class="modal-product__price">Price: ${i}</p>
         <button class="modal-product__buy-btn" type="button">Buy</button>
       </div>
-`}const s={categories:document.querySelector(".categories"),products:document.querySelector(".products"),productsItem:document.querySelector(".products__item"),categoriesBtn:document.querySelector(".categories__btn"),modal:document.querySelector(".modal"),modalProduct:document.querySelector(".modal-product"),modalCloseBtn:document.querySelector(".modal__close-btn"),notFound:document.querySelector(".not-found"),loadMoreBtn:document.querySelector(".load-more__btn")};function $(t,o){for(const e of t)if(e.firstElementChild.classList.contains("categories__btn--active")){e.firstElementChild.classList.remove("categories__btn--active");break}o.classList.add("categories__btn--active")}function u(t){t.length?s.notFound.classList.remove("not-found--visible"):s.notFound.classList.add("not-found--visible")}function m(t,o){if(o==="All"){p(t).then(e=>{u(e),s.products.insertAdjacentHTML("beforeend",d(e))}).catch(e=>console.log(e.message)),console.log(t);return}b(t,o).then(e=>{console.log(e),console.log(t),console.log(o),u(e),s.products.insertAdjacentHTML("beforeend",d(e))}).catch(e=>console.log(e.message))}const r=1;let l="All",i=r;//!================= home.js =================
-function v(t){if(!t.target.classList.contains("categories__btn"))return;i=r,s.products.innerHTML="";const o=Array.from(s.categories.children);$(o,t.target),l=t.target.textContent,m(r,l)}function M(){i++,m(i,l)}//!================= modal.js =================
-function A(t){if(t.target.classList.contains("products"))return;const o=Number(t.target.closest(".products__item").dataset.id);f(o).then(e=>{s.modalProduct.innerHTML=C(e),s.modal.classList.add("modal--is-open")}).catch(e=>console.log(e.message))}function B(){s.modal.classList.remove("modal--is-open")}s.categories.addEventListener("click",v);s.loadMoreBtn.addEventListener("click",M);h().then(t=>s.categories.insertAdjacentHTML("beforeend",L(["All",...t]))).catch(t=>alert(t.message)).finally(()=>{s.categories.firstElementChild.firstElementChild.classList.add("categories__btn--active")});p(r).then(t=>s.products.insertAdjacentHTML("beforeend",d(t))).catch(t=>console.log(t.message));s.products.addEventListener("click",A);s.modalCloseBtn.addEventListener("click",B);
+`}const s={categories:document.querySelector(".categories"),products:document.querySelector(".products"),productsItem:document.querySelector(".products__item"),categoriesBtn:document.querySelector(".categories__btn"),modal:document.querySelector(".modal"),modalProduct:document.querySelector(".modal-product"),modalCloseBtn:document.querySelector(".modal__close-btn"),notFound:document.querySelector(".not-found"),loadMoreBtn:document.querySelector(".load-more__btn"),loader:document.querySelector(".loader-js")};function B(t,e){for(const o of t)if(o.firstElementChild.classList.contains("categories__btn--active")){o.firstElementChild.classList.remove("categories__btn--active");break}e.classList.add("categories__btn--active")}function p(t){t.length?s.notFound.classList.remove("not-found--visible"):s.notFound.classList.add("not-found--visible")}function y(t,e){if(e==="All"){g(t).then(o=>{p(o),s.products.insertAdjacentHTML("beforeend",u(o))}).catch(o=>console.log(o.message));return}P(t,e).then(o=>{p(o),s.products.insertAdjacentHTML("beforeend",u(o))}).catch(o=>console.log(o.message))}//!================= LoadMore =================
+function h(){s.loadMoreBtn.classList.add("visually-hidden")}//!================= Loader =================
+function S(){s.loader.classList.remove("loader")}function k(){s.loader.classList.add("loader")}//!================= Loader =================
+function b(){alert("No more products!")}const d=1;let r="All",c=d,l=1;//!================= home.js =================
+function q(t){if(!t.target.classList.contains("categories__btn"))return;c=d,s.products.innerHTML="";const e=Array.from(s.categories.children);B(e,t.target),r=t.target.textContent,y(d,r),console.log(r),console.log(m(r)),m(r)===c&&console.log("?")}function E(){c++,k(),y(c,r),w(),S()}function w(){r==="All"?_().then(t=>{l=Math.ceil(Number(t)/12),c>=l&&(h(),c>2&&b())}).catch(t=>console.log(t.message)):m(r)}function m(t){f(t).then(e=>{l=Math.ceil(Number(e)/12),c>=l&&(h(),c>2&&b())}).catch(e=>console.log(e.message))}//!================= modal.js =================
+function T(t){if(t.target.classList.contains("products"))return;const e=Number(t.target.closest(".products__item").dataset.id);$(e).then(o=>{s.modalProduct.innerHTML=j(o),s.modal.classList.add("modal--is-open")}).catch(o=>console.log(o.message))}function H(){s.modal.classList.remove("modal--is-open")}s.categories.addEventListener("click",q);s.loadMoreBtn.addEventListener("click",E);v().then(t=>s.categories.insertAdjacentHTML("beforeend",A(["All",...t]))).catch(t=>alert(t.message)).finally(()=>{s.categories.firstElementChild.firstElementChild.classList.add("categories__btn--active")});g(d).then(t=>s.products.insertAdjacentHTML("beforeend",u(t))).catch(t=>console.log(t.message));_();f("groceries");s.products.addEventListener("click",T);s.modalCloseBtn.addEventListener("click",H);
 //# sourceMappingURL=index.js.map
